6f90a57bb96624ae13de38eff023a85800a3a699
Gurcharan Singh <gurcharansingh2226@gmail.com>
2024-02-08 17:34:13 -0500

    server on 8011 page

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Message represent a simple JSON message.
+type Message struct {
+	Greeting string `json:"greeting"` // Greeting is the greeting field in the JSON message
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+
+	message := &Message{Greeting: "Hello World!"}
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+func main() {
+
+	http.HandleFunc("/welcome", greetHandler)
+	http.ListenAndServe(":8011", nil)
+
+}
